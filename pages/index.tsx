381d03e97ddef0c972c4e4e32ff9c4ccf575d56d
kh <rlaruagh@gmail.com>
2022-04-26 09:04:35 +0900

    little

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home({ collections }: Props) {
 						transition-all duration-200 hover:scale-105 '>
 								<img
 									className='h-96 w-60 rounded-2xl object-cover'
-									src={urlFor(collection.previewImage).url()}
+									src={urlFor(collection.mainImage).url()}
 									alt='cannot resolve'
 								/>
 								<div>
